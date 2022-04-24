c9e83eae85355f9dd545af9fe52e16fb4ccf1e31
Krystian <kris.akuzino@gmail.com>
2022-04-24 12:48:58 +0200

    Updates

---
+++ precache-manifest.23e24568c51474d94bc1e0d59eebcd98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7005b767cfb6e4b46921",
+    "url": "/strona/static/css/main.c99b21cc.chunk.css"
+  },
+  {
+    "revision": "7005b767cfb6e4b46921",
+    "url": "/strona/static/js/main.7005b767.chunk.js"
+  },
+  {
+    "revision": "b38147032484e642abff",
+    "url": "/strona/static/js/runtime~main.b3814703.js"
+  },
+  {
+    "revision": "c580d1078671b0daa5b5",
+    "url": "/strona/static/js/2.c580d107.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/strona/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/strona/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/strona/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "b713bb112c0e9b7ac3e96c5fc90b92c6",
+    "url": "/strona/index.html"
+  }
+];
